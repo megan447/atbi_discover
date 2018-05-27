bdbbbaa5b5f7d9afae507d6b75769f3471a29b26
ericlan <lanzhuoqin@gmail.com>
2018-05-27 18:04:30 -0400

    added doc example.

--- api/post.js
+++ api/post.js
@@ -2,7 +2,24 @@ const express = require('express');
 const router = express.Router();
 let postController = require('../controllers/postController');
 
-// post API
+/**
+ * @api {get} discover/getPosts get all posts
+ * @apiSampleRequest http://localhost:5555/api/discover/getPosts
+ * @apiName getAll
+ * @apiVersion 0.1.0
+ * @apiGroup Discover/Post
+ * @apiSuccess {boolean} success server response status.
+ * @apiSuccess {String} message  server message.
+ * @apiSuccess {[Object]} posts List.
+
+ * @apiSuccessExample Success-true:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": true,
+ *       "message": "Get posts successfully"
+ *       "posts": [{...},{...},{...}]
+ *     }
+ */
 router.get('/getPosts', postController.getPosts);
 
 router.get('/getPostById', postController.getPostById);
