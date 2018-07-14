ce58bf55e8b811a6f3f05682c648c9f41e58c842
Lu <xiemegan447@gmail.com>
2018-07-14 17:02:31 -0400

    lazy loading successful

--- ATBI/src/app/route.module.ts
+++ ATBI/src/app/route.module.ts
@@ -3,12 +3,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {NgMaterialModule} from './ngMaterial.module';
-import {HomeComponent} from "./pages/home/home.component";
 import {DetailComponent} from "./pages/detail/detail.component";
 import {ProfileComponent} from "./pages/profile/profile.component";
 
 const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', loadChildren: './pages/home/home.module#HomeModule'},
     {path: 'home/detail/:id', component: DetailComponent},
     {path: 'home/profile/:id', component: ProfileComponent},
 ];
